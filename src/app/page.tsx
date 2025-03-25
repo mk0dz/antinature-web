1cbe339d14a3856b5ec5ba8b98fbfe26d061376f
Makul0 <MukulKumar2027@gmail.com>
2025-03-25 06:41:50 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
             </div>
           )}
         </div>
-        <a href="https://doi.org/10.5281/zenodo.15079747"><img src="https://zenodo.org/badge/DOI/10.5281/zenodo.15079747.svg" alt="DOI" className='w-10 h-10'></img></a>
+       
         <pre className="overflow-x-auto relative inline-flex items-center mt-4 justify-center border border-gray-300 bg-white px-3 sm:px-6 py-2 sm:py-3 font-medium text-black hover:bg-gray-50 md:px-4 md:py-2 w-full md:w-auto">
             <code className='relative text-sm sm:text-base md:text-lg lg:text-xl tracking-wide'>
                 from antinature.core import MixedMatterBasis <br />
